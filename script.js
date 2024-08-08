f5989ee040529ddf9045ea25ba792f4dfff1f579
Alfredo Dheryk <dheryk@gmail.com>
2024-08-08 10:16:54 -0300

    Primeiro commit

---
+++ script.js
@@ -0,0 +1,9 @@
+//Q5
+//let itens = document.getElementsByClassName('item')
+
+//console.log(itens)
+
+//Q06
+//let itens = document.getElementsByClassName('item')
+
+//itens[0].innerHTML = "Goiaba"
